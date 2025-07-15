76678e4710480a740c998ed53e9937dba9521308
KianaMirayi <1916599843@qq.com>
2025-07-16 00:16:06 +0800

    Summary

--- Assets/Scripts/Characters/Player/Data/States/Grounded/Moving/PlayerSprintData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Grounded/Moving/PlayerSprintData.cs
@@ -9,8 +9,8 @@ namespace GenshinImpactMovementSystem
     public class PlayerSprintData
     {
         [field: SerializeField][field: Range(1f, 3f)] public float sprintSpeedModifier { get; private set; } = 1.7f;
-        [field: SerializeField][field: Range(0f, 5f)] public float sprintToRunTime { get; private set; } = 1f;
-        [field: SerializeField][field: Range(0f, 2f)] public float runToWalkTime { get; private set; } = 0.5f;
+        [field: SerializeField][field: Range(0f, 5f)] public float sprintToRunTime { get; private set; } = 1f; //疾跑状态自动切换到奔跑状态的持续时间
+        [field: SerializeField][field: Range(0f, 2f)] public float runToWalkTime { get; private set; } = 0.5f;//奔跑状态自动切换到走路状态的持续时间
 
 
     }
--- Assets/Scripts/Characters/Player/Data/States/Grounded/PlayerDashData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Grounded/PlayerDashData.cs
@@ -10,8 +10,8 @@ namespace GenshinImpactMovementSystem
     {
         [field: SerializeField][field: Range(1f, 3f)] public float DashSpeedModifier { get; private set; } = 2f;
         [field: SerializeField] public PlayerRotationData RotationData { get; private set; }    
-        [field: SerializeField][field: Range(0f, 2f)] public float TimeToBeConsideredConsecutive { get; private set; } = 1f; //冲刺时间
+        [field: SerializeField][field: Range(0f, 2f)] public float TimeToBeConsideredConsecutive { get; private set; } = 1f; //连续冲刺时间窗口
         [field: SerializeField][field: Range(1, 10)] public int ConsecutiveDashesLimitAmount { get; private set; } = 2;  //可连续冲刺次数
-        [field: SerializeField][field: Range(1f, 5f)] public float DashLimitReachedCooldown { get; private set; } = 1.75f;
+        [field: SerializeField][field: Range(1f, 5f)] public float DashLimitReachedCooldown { get; private set; } = 1.75f; //禁用连续冲刺时间冷却
     }
 }
